409515ef530254c1ba125aa20948d9df0a3a3eb8
DigohD <digohd1337@gmail.com>
2015-01-21 17:41:57 +0100

    MP Init

---
+++ Assets/MyScripts/CollisionTest.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+@script RequireComponent(PhysController2D)
+
+public var Sparks1 : GameObject;
+
+function OnCollisionEnter (col : Collision){
+	if(col.gameObject.name == "Capsule"){
+		//var exp : GameObject = Instantiate (Sparks1, col.gameObject.transform.position, col.gameObject.transform.rotation);
+ 		//exp.particleEmitter.Emit();
+  		//Destroy (exp, 2);
+ 		
+        //Destroy(col.gameObject);
+    }
+    
+    
+	
+	Debug.Log("COLLIDE!!!!!!");
+}
---
+++ Assets/MyScripts/Network/NetworkHandler.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function OnGUI(){
+
+	if(GUI.Button(new Rect(25f, 25f, 150f, 30f), "Start Server")){
+		//DFHSDFHSDFH
+	}
+	
+	if(GUI.Button(new Rect(25f, 65f, 150f, 30f), "Server List")){
+		//DFHSDFHSDFH
+	}
+
+}
