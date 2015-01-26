83074afb658c631d2f721b71c2ffbec5b320c5f2
Adamsc <sco3.1415@gmail.com>
2015-01-26 20:00:42 +0100

    Magnet prototype
    Some scripts to handle a magnetic pull to the bubble

---
+++ Assets/MyScripts/AttachTrash.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachTrash : MonoBehaviour {
+
+	public GameObject player;
+
+	void OnTriggerEnter( Collider other)
+	{
+		print ("Trying to connect1");
+		if (other.tag == "Trash") 
+		{
+			//Set the piece of thrash as a child to the gameobject
+			other.transform.parent = player.transform;
+
+			//Set up the joint
+			FixedJoint joint;
+			joint = player.gameObject.AddComponent<FixedJoint>();
+			joint.connectedBody = other.rigidbody;
+			other.tag = "BodyPart";
+
+
+			//other.rigidbody.velocity = player.rigidbody.velocity;
+			//print ("Trying to connect2");
+			//player.GetComponent<FixedJoint>().connectedBody = other.rigidbody;
+
+
+		}
+
+	}
+	                                  
+}
---
+++ Assets/MyScripts/PlayerControlScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlScript : MonoBehaviour {
+
+	public float speed;
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal , moveVertical, 0.0f); 
+		rigidbody.velocity = movement*speed;
+	}
+
+}
