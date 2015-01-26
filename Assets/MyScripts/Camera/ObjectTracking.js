575eacf52a3ea2e3f997b8d43a4902f86a1cfdfe
DigohD <digohd1337@gmail.com>
2015-01-26 20:35:58 +0100

    Object Following Commenting

--- Assets/MyScripts/Camera/ObjectTracking.js
+++ Assets/MyScripts/Camera/ObjectTracking.js
@@ -1,41 +1,46 @@
 ﻿#pragma strict
 
+// The game object to follow
 var tracked : GameObject;
+// The follow "speed", less acceleration leads to elastic follow movement
 var acceleration : float = 0.05f;
+// The distance from the camera to the object
+var baseDistance : float = 10f;
+// The amount the camera zooms out due to speed of the object
+var zoomAmount : float = 25f;
 
 private var velocity = Vector2(0, 0);
 
 function Start()
 {
+	//Initialize the camera centered on the tracked object
 	gameObject.transform.position = Vector3(
-	tracked.transform.position.x,
-	tracked.transform.position.y,
-	tracked.transform.position.z - 10
-	);
+			tracked.transform.position.x,
+			tracked.transform.position.y,
+			tracked.transform.position.z - baseDistance);
 }
 
 function Update()
 {
-
-	/*gameObject.transform.position = Vector3(
-	tracked.transform.position.x,
-	tracked.transform.position.y,
-	tracked.transform.position.z - 10
-	);*/
-	var myPos : Vector3 = Vector3(
-			gameObject.transform.position.x,
-			gameObject.transform.position.y,
-			gameObject.transform.position.z);
+	// Position of the camera
+	var myPos : Vector3 = gameObject.transform.position;
+	
+	// Position that the camera moves towards
 	var targetPos : Vector3 = Vector3(
 			tracked.transform.position.x,
 			tracked.transform.position.y,
-			tracked.transform.position.z - 10);
+			tracked.transform.position.z - baseDistance);
 	
-	//var distance : float = (tracked.transform.position.x - gameObject.transform.position.x) +
-	//		(tracked.transform.position.y - gameObject.transform.position.y);
-	
+	// The higher the distance between myPos and targetPos, the higher the speed
 	velocity = Vector3(targetPos.x - myPos.x, targetPos.y - myPos.y, targetPos.z - myPos.y) * acceleration;
 	
+	// Zoom out amount, depending on speed
+	var extraDistance : float = velocity.magnitude * zoomAmount;
+	
+	// Set the new camera position by adding velocity
 	gameObject.transform.position = gameObject.transform.position + velocity;
+	
+	// Set depth of Camera, adding extra distance
+	gameObject.transform.position.z = tracked.transform.position.z - baseDistance - extraDistance;
 	
 }
