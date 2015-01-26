61dbd3c588704da7743a436625676036c3bb750d
Adamsc <sco3.1415@gmail.com>
2015-01-26 20:39:41 +0100

    magnets
    now working

---
+++ Assets/TrashToTrashConnector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashToTrashConnector : MonoBehaviour {
+
+	public GameObject player;
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Trash") 
+		{
+			//Set the piece of thrash as a child to the gameobject
+			other.transform.parent = player.transform;
+			
+			//Set up the joint
+			FixedJoint joint;
+			joint = player.gameObject.AddComponent<FixedJoint> ();
+			joint.connectedBody = other.rigidbody;
+			other.tag = "BodyPart";
+		}
+	}
+}
