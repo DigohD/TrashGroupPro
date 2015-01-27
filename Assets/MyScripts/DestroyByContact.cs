29c9d3261b9a4a3a2823cf4ae2a1cc9a66b861f3
Adamsc <sco3.1415@gmail.com>
2015-01-27 13:44:53 +0100

    Hazards
    Added exploding gas bottles that propells the player and destroys the
    object that touches it ( will be improved to destroy everyting in the
    radius)

---
+++ Assets/MyScripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player" || other.tag == "BodyPart") {
+
+			Instantiate(explosion, transform.position, transform.rotation);
+			Destroy(this.gameObject);
+			Destroy (other.gameObject);
+		
+		}
+
+	}
+}
