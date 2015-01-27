d13f459ea1aa9b20310e12ff0b7ab3bbb93119df
Adamsc <sco3.1415@gmail.com>
2015-01-27 12:57:26 +0100

    Trashimprovements
    Better joints for the trash to trash connections. Severing a piece of
    trash will also take away it's connected children from the main part.
    Also created a simple trashspawner for easier testing

--- Assets/MyScripts/TrashToTrashConnector.cs
+++ Assets/MyScripts/TrashToTrashConnector.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class TrashToTrashConnector : MonoBehaviour {
 
-	public GameObject player;
+	//public GameObject player;
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.tag == "Trash") 
+		if (other.tag == "Trash" && this.tag == "BodyPart") 
 		{
-			//Set the piece of thrash as a child to the gameobject
-			other.transform.parent = player.transform;
+			//Set the piece of thrash as a child to the player gameobject
+			other.transform.parent = this.transform.parent;
 			
 			//Set up the joint
 			FixedJoint joint;
-			joint = player.gameObject.AddComponent<FixedJoint> ();
+			joint = this.gameObject.AddComponent<FixedJoint> ();
 			joint.connectedBody = other.rigidbody;
 			other.tag = "BodyPart";
 		}
---
+++ Assets/TrashSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashSettings : MonoBehaviour {
+
+	// Use this for initialization
+	public float health;
+	public float scale;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (health == 0)
+						Destroy (this);
+	}
+}
