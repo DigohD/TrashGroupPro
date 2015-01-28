ffe95593b7b5ad2a8df84286b31709a3848215bb
Adamsc <sco3.1415@gmail.com>
2015-01-28 11:43:03 +0100

    Simple enemy
    Added a simple "enemy" that the player can kill if hit sufficiently

--- Assets/MyScripts/Player/PlayerStats.cs
+++ Assets/MyScripts/Player/PlayerStats.cs
@@ -4,14 +4,21 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour {
 
 	public float speed;
+	public float dmg;
 	
 	// Update is called once per frame
 	void start () {
 		speed = 3;
+		dmg = 2;
 	}
 
 	public void addAttributes(float nSpeed)
 	{
 		speed += nSpeed;
+	}
+	public void addAttributes(float nSpeed, float nDamage)
+	{
+		speed += nSpeed;
+		dmg += nDamage;
 	}
 }
