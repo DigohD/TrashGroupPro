8a9ca6708be8f0bdd479497ce1e4ec2d339b84b6
Adamsc <sco3.1415@gmail.com>
2015-02-02 15:21:23 +0100

    Player rotation
    Implemented controls for rotating the player towards the mouse, also
    added controls that allow for steering through wasd as well as rotating
    by q&e

--- Assets/MyScripts/Player/PlayerControlScript.cs
+++ Assets/MyScripts/Player/PlayerControlScript.cs
@@ -4,13 +4,60 @@ using System.Collections;
 public class PlayerControlScript : MonoBehaviour {
 
 	public PlayerStats stats;
-	void FixedUpdate(){
+	public Camera cam;
+
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+	private Vector3 mouse;
+	private Vector3 screenPoint;
+	private Vector2 offset;
+	private float angle;
+
+
+	void Update() {
+		if (Input.GetKey ("w"))
+						moveVertical = 1;
+				else if (Input.GetKey ("s"))
+						moveVertical = -1;
+				else {
+						moveVertical = 0;
+				}
+		if (Input.GetKey ("d"))
+						moveHorizontal = 1;
+				else if (Input.GetKey ("a"))
+						moveHorizontal = -1;
+				else {
+						moveHorizontal = 0;
+				}
+		//uncomment to allow roation by q&e
+		/*if (Input.GetKey ("e"))
+			rotate = 1;
+		else if (Input.GetKey ("q"))
+			rotate = -1;
+		else {
+			rotate = 0;
+		}*/
+
+		movement = new Vector3(moveHorizontal , moveVertical, 0.0f); 
+		rigidbody.velocity = movement*stats.speed;
+		//transform.Rotate(new Vector3(0,0,rotate*3)); //Uncomment to allow rotation by q&e
+
+		mouse = Input.mousePosition;
+		screenPoint = cam.WorldToScreenPoint(transform.localPosition);
+		offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+		angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0, 0, angle);
+	
+	}
+
+	/*void FixedUpdate(){
 
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3(moveHorizontal , moveVertical, 0.0f); 
 		rigidbody.velocity = movement*stats.speed;
-	}
+	}*/
 
 }
