c346c17989ca71e7fe9ef64b0641823bfac0ada8
Adamsc <sco3.1415@gmail.com>
2015-02-10 17:15:09 +0100

    bb for shots etc

---
+++ Assets/shotOutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shotOutOfBounds : MonoBehaviour {
+
+	void OnTriggerExit( Collider other) {
+		print ("jndffndsf");
+		if (other.tag == "Gunshot")
+						Destroy (other.gameObject);
+
+
+	}
+}
