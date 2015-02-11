c933b66efe0cadf200993c56a36dc074572a0e1a
Z3B0 <sebastian.odbjer@gmail.com>
2015-02-11 21:06:05 +0100

    Map and underwater effects testing
    Created a terrain and added fog and underwater caustics to test out how
    to achieve an underwater "feel".

--- Assets/MyScripts/Camera/ObjectTracking.js
+++ Assets/MyScripts/Camera/ObjectTracking.js
@@ -3,11 +3,11 @@
 // The game object to follow
 var tracked : GameObject;
 // The follow "speed", less acceleration leads to elastic follow movement
-var acceleration : float = 0.05f;
+var acceleration : float = 0.1f;
 // The distance from the camera to the object
-var baseDistance : float = 10f;
+var baseDistance : float = 5f;
 // The amount the camera zooms out due to speed of the object
-var zoomAmount : float = 25f;
+var zoomAmount : float = 5f;
 
 private var velocity = Vector2(0, 0);
 
@@ -18,6 +18,7 @@ function Start()
 			tracked.transform.position.x,
 			tracked.transform.position.y,
 			tracked.transform.position.z - baseDistance);
+	
 }
 
 private var myPos : Vector3;
@@ -43,7 +44,7 @@ function FixedUpdate()
 	extraDistance = velocity.magnitude * zoomAmount;
 	
 	// Set the new camera position by adding velocity
-	gameObject.transform.position = gameObject.transform.position + velocity;
+	gameObject.transform.position = targetPos + velocity;
 	
 	// Set depth of Camera, adding extra distance
 	gameObject.transform.position.z = tracked.transform.position.z - baseDistance - extraDistance;
