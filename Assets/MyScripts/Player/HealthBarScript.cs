7502783e7e47b457e6d619fd32a0afaff7dcf6ca
DigohD <digohd1337@gmail.com>
2015-02-11 10:41:23 +0100

    HealthBar

---
+++ Assets/MyScripts/Player/HealthBarScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarScript : MonoBehaviour {
+
+	public GameObject healthBarR;
+
+	private PlayerStats stats;
+	private GameObject healthBar;
+	private GameObject healthBar2;
+
+	private float healthPercent = 0.7f;
+
+	void Start () {
+		healthBar = (GameObject) Instantiate(healthBarR, gameObject.transform.position, Quaternion.identity);
+
+		foreach (Transform child in healthBar.transform)
+		{
+			if(child.gameObject.CompareTag("HealthBarG")){
+				healthBar2 = child.gameObject;
+			}
+		}
+
+		stats = (PlayerStats) gameObject.GetComponent("PlayerStats");
+
+		healthBar.transform.position = gameObject.transform.position + new Vector3(0f, 1.3f, -1f);
+		healthBar.transform.localScale = new Vector3(10f, 1f, 1f);
+		healthBar2.transform.localScale = new Vector3(healthPercent, 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthBar.transform.position = gameObject.transform.position + new Vector3(0f, 1.3f, -1f);
+
+		healthBar.transform.localScale = new Vector3(10f, 1f, 1f);
+		healthBar2.transform.localScale = new Vector3(healthPercent, 1f, 1f);
+	}
+
+	void OnDestroy(){
+		Destroy(healthBar);
+	}
+}
