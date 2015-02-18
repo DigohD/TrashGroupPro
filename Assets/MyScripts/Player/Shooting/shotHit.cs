87081a04e8829030dd22ba4cf823d90941e60d7b
Adamsc <sco3.1415@gmail.com>
2015-02-18 17:28:58 +0100

    error fix

--- Assets/MyScripts/Player/Shooting/shotHit.cs
+++ Assets/MyScripts/Player/Shooting/shotHit.cs
@@ -12,7 +12,7 @@ public class shotHit : MonoBehaviour {
 		dmg = 30;
 		if (other.tag == "Player") {
 			PlayerStats EnemyStats = other.GetComponent<PlayerStats> ();
-			if(!sender.Length<3 && EnemyStats.ID != null && !EnemyStats.ID.Equals(this.sender)){
+			if(!(sender.Length < 3) && EnemyStats.ID != null && !EnemyStats.ID.Equals(this.sender)){
 				Debug.Log(this.sender + " hit: " + EnemyStats.ID);
 				EnemyStats.takeDamage(dmg);
 				//Destroy(other.gameObject);
