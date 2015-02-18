ad333159ceccdcaf584bfb8ef574578a7f4152c5
Z3B0 <sebastian.odbjer@gmail.com>
2015-02-18 15:45:02 +0100

    Merge conflict thingy

--- Assets/MyScripts/Camera/ObjectTrack.cs
+++ Assets/MyScripts/Camera/ObjectTrack.cs
@@ -6,9 +6,9 @@ public class ObjectTrack : MonoBehaviour {
 	// The game object to follow
 	public GameObject tracked;
 	// The follow "speed", less acceleration leads to elastic follow movement
-	float acceleration = 0.1f;
+	float acceleration = 0.05f;
 	// The distance from the camera to the object
-	float baseDistance = 3f;
+	float baseDistance = 10f;
 	// The amount the camera zooms out due to speed of the object
 	float zoomAmount = 30f;
 
@@ -52,7 +52,6 @@ public class ObjectTrack : MonoBehaviour {
 			
 			// Set the new camera position by adding velocity
 			gameObject.transform.position = (gameObject.transform.position + velocity);
-
 
 			if((roof.transform.position.y - roofOffset) <= gameObject.transform.position.y)
 			{
