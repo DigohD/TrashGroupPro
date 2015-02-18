088db3f6d193cd9bd90163e249cc37d651a6937a
DigohD <digohd1337@gmail.com>
2015-02-18 11:39:11 +0100

    Trash Attach fix?

--- Assets/MyScripts/Player/AttachTrash.cs
+++ Assets/MyScripts/Player/AttachTrash.cs
@@ -10,20 +10,35 @@ public class AttachTrash : MonoBehaviour {
 
 		if (other.tag == "Trash" && stats.magnetOn) 
 		{
-			//Set the piece of thrash as a child to the player gameobject
-			other.transform.parent = this.transform;
+			if(Network.isClient){
+				Transform t = other.transform;
+				GameObject newTrash = (GameObject) Network.Instantiate(other, t.position, t.rotation, 0);
+				GameObject.Destroy(other);
 
-			TrashStats tStats = other.GetComponent<TrashStats>(); //Add attributes to player
-			stats.addAttributes(tStats.speed);
-
-			//Set up the joint
-			FixedJoint joint;
-			joint = this.gameObject.AddComponent<FixedJoint>();
-			joint.connectedBody = other.rigidbody;
-			other.tag = "BodyPart";
-
-
-
+				//Set the piece of thrash as a child to the player gameobject
+				newTrash.transform.parent = this.transform;
+				
+				TrashStats tStats = newTrash.GetComponent<TrashStats>(); //Add attributes to player
+				stats.addAttributes(tStats.speed);
+				
+				//Set up the joint
+				FixedJoint joint;
+				joint = this.gameObject.AddComponent<FixedJoint>();
+				joint.connectedBody = newTrash.rigidbody;
+				newTrash.tag = "BodyPart";
+			}else{
+				//Set the piece of thrash as a child to the player gameobject
+				other.transform.parent = this.transform;
+				
+				TrashStats tStats = other.GetComponent<TrashStats>(); //Add attributes to player
+				stats.addAttributes(tStats.speed);
+				
+				//Set up the joint
+				FixedJoint joint;
+				joint = this.gameObject.AddComponent<FixedJoint>();
+				joint.connectedBody = other.rigidbody;
+				other.tag = "BodyPart";
+			}
 		}
 
 	}
