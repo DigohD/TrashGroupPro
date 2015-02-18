e5dcfedca254c1a76c3b17225647415c754af55c
Adamsc <sco3.1415@gmail.com>
2015-02-18 13:36:38 +0100

    network

--- Assets/MyScripts/Player/AttachTrash.cs
+++ Assets/MyScripts/Player/AttachTrash.cs
@@ -9,7 +9,7 @@ public class AttachTrash : MonoBehaviour {
 
 	void OnTriggerEnter( Collider other)
 	{
-		if (other.tag == "Trash" && stats.magnetOn) 
+		if (other.tag == "Trash" && stats.magnetOn && transform.GetComponent<NetworkView>().isMine) 
 		{
 			//Set the piece of thrash as a child to the player gameobject
 			other.transform.parent = this.transform;
