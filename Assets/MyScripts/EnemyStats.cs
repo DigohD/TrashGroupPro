5284e5af064684985259783dda7a16c56bb3b2a4
Adamsc <sco3.1415@gmail.com>
2015-02-26 17:57:47 +0100

    Blood when shooting fishy
    the visual of the blood is still work in progress

--- Assets/MyScripts/EnemyStats.cs
+++ Assets/MyScripts/EnemyStats.cs
@@ -8,6 +8,7 @@ public class EnemyStats : MonoBehaviour {
 	private float speed;
 	private float happiness;
 	public GameObject drop;
+	public GameObject blood;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +17,10 @@ public class EnemyStats : MonoBehaviour {
 
 	public void takeDamage (float damage) {
 		health -= damage;
+
+		Network.Instantiate (blood, gameObject.transform.position, blood.transform.rotation, 0);
 		if (health <= 0){
 			Vector3 pos = this.gameObject.transform.position;
-
 			Destroy (this.gameObject);
 			if(drop != null);
 			GameObject t = (GameObject) Network.Instantiate (drop, pos, drop.transform.rotation, 0);
